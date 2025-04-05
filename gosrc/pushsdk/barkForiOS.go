4e2cd3daca6134ea9bf1d88faa299601b7e32271
Patrick Young <16604643+kmahyyg@users.noreply.github.com>
2025-04-05 16:22:51 +0800

    migrate and split Bark for iOS code

---
+++ gosrc/pushsdk/barkForiOS.go
@@ -0,0 +1,79 @@
+package pushsdk
+
+// BarkPushContent is an instance of https://github.com/Finb/bark-server/blob/master/docs/API_V2.md
+// current version is v2.2.0 for server side, updated at 2025/04/05
+type BarkPushContent struct {
+	// required
+	Title     string `json:"title" validate:"required"`
+	Body      string `json:"body" validate:"required"`
+	DeviceKey string `json:"device_key" validate:"required"`
+	// from here, all options below are OPTIONAL
+	Level BarkiOSNotificationLevel `json:"level,omitempty"`
+	Badge int                      `json:"badge,omitempty" validate:"omitempty,gt=0"`
+	// AutomaticallyCopy must be "1" in string
+	AutomaticallyCopy string `json:"automaticallyCopy,omitempty" validate:"omitempty,oneof='0' '1'"`
+	// Copy store the value to be copied when user click button
+	Copy string `json:"copy,omitempty"`
+	// alert Sound, from https://github.com/Finb/Bark/tree/master/Sounds
+	Sound BarkAlertSound `json:"sound,omitempty"`
+	// Notification Icon URL
+	Icon string `json:"icon,omitempty"`
+	// Group Seperation
+	Group string `json:"group,omitempty"`
+	// IsArchive must be string, "1" or "0"
+	IsArchive string `json:"isArchive,omitempty" validate:"omitempty,oneof='0' '1'"`
+	// Jump to URL when clicked
+	URL string `json:"url,omitempty" validate:"omitempty,url"`
+}
+
+func (bpct *BarkPushContent) Init() {
+	bpct.AutomaticallyCopy = "1"
+	bpct.IsArchive = "1"
+	bpct.Level = ActiveNotification
+}
+
+type BarkiOSNotificationLevel string
+
+const (
+	ActiveNotification        BarkiOSNotificationLevel = "active"
+	TimeSensitiveNotification BarkiOSNotificationLevel = "timeSensitive"
+	SilentNotification        BarkiOSNotificationLevel = "passive"
+	CriticalNotification      BarkiOSNotificationLevel = "critical"
+)
+
+type BarkAlertSound string
+
+const (
+	Alarm              BarkAlertSound = "alarm.caf"
+	Anticipate         BarkAlertSound = "anticipate.caf"
+	Bell               BarkAlertSound = "bell.caf"
+	Birdsong           BarkAlertSound = "birdsong.caf"
+	Bloom              BarkAlertSound = "bloom.caf"
+	Calypso            BarkAlertSound = "calypso.caf"
+	Chime              BarkAlertSound = "chime.caf"
+	Choo               BarkAlertSound = "choo.caf"
+	Descent            BarkAlertSound = "descent.caf"
+	Electronic         BarkAlertSound = "electronic.caf"
+	Fanfare            BarkAlertSound = "fanfare.caf"
+	Glass              BarkAlertSound = "glass.caf"
+	Gotosleep          BarkAlertSound = "gotosleep.caf"
+	Healthnotification BarkAlertSound = "healthnotification.caf"
+	Horn               BarkAlertSound = "horn.caf"
+	Ladder             BarkAlertSound = "ladder.caf"
+	Mailsent           BarkAlertSound = "mailsent.caf"
+	Minuet             BarkAlertSound = "minuet.caf"
+	Multiwayinvitation BarkAlertSound = "multiwayinvitation.caf"
+	Newmail            BarkAlertSound = "newmail.caf"
+	Newsflash          BarkAlertSound = "newsflash.caf"
+	Noir               BarkAlertSound = "noir.caf"
+	Paymentsuccess     BarkAlertSound = "paymentsuccess.caf"
+	Shake              BarkAlertSound = "shake.caf"
+	Sherwoodforest     BarkAlertSound = "sherwoodforest.caf"
+	Silence            BarkAlertSound = "silence.caf"
+	Spell              BarkAlertSound = "spell.caf"
+	Suspense           BarkAlertSound = "suspense.caf"
+	Telegraph          BarkAlertSound = "telegraph.caf"
+	Tiptoes            BarkAlertSound = "tiptoes.caf"
+	Typewriters        BarkAlertSound = "typewriters.caf"
+	Update             BarkAlertSound = "update.caf"
+)
--- gosrc/pushsdk/models.go
+++ gosrc/pushsdk/models.go
@@ -8,63 +8,20 @@ import (
 
 var verifier = validator.New()
 
-type pusher struct {
-	Config   *PushConfig
-	l        *DumbLogger
-	Contents []*PushContent
-}
-
-func NewPusher(conf *PushConfig, logger *DumbLogger) (*pusher, error) {
-	err := verifier.Struct(conf)
-	if err != nil {
-		return nil, err
-	}
-	return &pusher{
-		Config:   conf,
-		l:        logger,
-		Contents: nil,
-	}, nil
-}
-
 type PushConfig struct {
-	DeviceKeys        []string          `json:"deviceKeys" validate:"required"`
-	ServerAddress     string            `json:"serverAddress" validate:"url,required"`
-	NotificationLevel NotificationLevel `json:"notificationLevel,omitempty" validate:"omitempty,oneof='active' 'passive' 'timeSensitive'"`
+	PushMethods []string `json:"pushMethods" validate:"required"`
+	IsDryRun    bool     `json:"isDryRun" validate:"boolean,required"`
+
+	// ServChan 3 for mobile universal
+	SC3ServerURL   string   `json:"servChan3ServerURL" validate:"url,omitempty"`
+	CustomPushTags []string `json:"servChan3CustomPushTags" validate:"omitempty"`
+	// Bark for ios only
+	BarkDeviceKeys           []string                 `json:"barkDeviceKeys"`
+	BarkServerURL            string                   `json:"barkServerURL" validate:"url,omitempty"`
+	BarkiOSNotificationLevel BarkiOSNotificationLevel `json:"barkiOSNotificationLevel,omitempty" validate:"omitempty,oneof='active' 'passive' 'timeSensitive'"`
 }
 
-// PushContent is an instance of https://github.com/Finb/bark-server/blob/master/docs/API_V2.md
-type PushContent struct {
-	// required
-	Title     string `json:"title" validate:"required"`
-	Body      string `json:"body" validate:"required"`
-	Category  string `json:"category" validate:"len=0"`
-	DeviceKey string `json:"device_key" validate:"required"`
-	// optional, from here
-	Level NotificationLevel `json:"level,omitempty"`
-	Badge int               `json:"badge,omitempty" validate:"omitempty,gt=0"`
-	// must be "1"
-	AutomaticallyCopy string `json:"automaticallyCopy,omitempty" validate:"omitempty,oneof='0' '1'"`
-	// value to be copied
-	Copy string `json:"copy,omitempty"`
-	// alert sound, from https://github.com/Finb/Bark/tree/master/Sounds
-	Sound AlertSound `json:"sound,omitempty"`
-	// Notification Icon URL
-	Icon string `json:"icon,omitempty"`
-	// Group Seperation
-	Group string `json:"group,omitempty"`
-	// isArchive must be string, "1" or "0"
-	IsArchive string `json:"isArchive,omitempty" validate:"omitempty,oneof='0' '1'"`
-	// Jump to URL when clicked
-	URL string `json:"url,omitempty" validate:"omitempty,url"`
-}
-
-func (pct *PushContent) Init() {
-	pct.AutomaticallyCopy = "1"
-	pct.IsArchive = "1"
-	pct.Category = ""
-	pct.Level = ActiveNotification
-}
-
+// PushResponse represent HTTP Response Data from PushNotification Service Provider
 type PushResponse struct {
 	Code      int
 	Message   string
@@ -76,47 +33,19 @@ func (p *PushResponse) String() string {
 	return fmt.Sprintf("Response %d: %s at time %s", p.Code, p.Message, t1.Format(time.RFC3339))
 }
 
-type NotificationLevel string
+// pusher is general instance for storing push config and responsible for further data transfer
+type pusher struct {
+	Config   *PushConfig
+	Contents []*BarkPushContent
+}
 
-const (
-	ActiveNotification        NotificationLevel = "active"
-	TimeSensitiveNotification NotificationLevel = "timeSensitive"
-	SilentNotification        NotificationLevel = "passive"
-)
-
-type AlertSound string
-
-const (
-	Alarm              AlertSound = "alarm.caf"
-	Anticipate         AlertSound = "anticipate.caf"
-	Bell               AlertSound = "bell.caf"
-	Birdsong           AlertSound = "birdsong.caf"
-	Bloom              AlertSound = "bloom.caf"
-	Calypso            AlertSound = "calypso.caf"
-	Chime              AlertSound = "chime.caf"
-	Choo               AlertSound = "choo.caf"
-	Descent            AlertSound = "descent.caf"
-	Electronic         AlertSound = "electronic.caf"
-	Fanfare            AlertSound = "fanfare.caf"
-	Glass              AlertSound = "glass.caf"
-	Gotosleep          AlertSound = "gotosleep.caf"
-	Healthnotification AlertSound = "healthnotification.caf"
-	Horn               AlertSound = "horn.caf"
-	Ladder             AlertSound = "ladder.caf"
-	Mailsent           AlertSound = "mailsent.caf"
-	Minuet             AlertSound = "minuet.caf"
-	Multiwayinvitation AlertSound = "multiwayinvitation.caf"
-	Newmail            AlertSound = "newmail.caf"
-	Newsflash          AlertSound = "newsflash.caf"
-	Noir               AlertSound = "noir.caf"
-	Paymentsuccess     AlertSound = "paymentsuccess.caf"
-	Shake              AlertSound = "shake.caf"
-	Sherwoodforest     AlertSound = "sherwoodforest.caf"
-	Silence            AlertSound = "silence.caf"
-	Spell              AlertSound = "spell.caf"
-	Suspense           AlertSound = "suspense.caf"
-	Telegraph          AlertSound = "telegraph.caf"
-	Tiptoes            AlertSound = "tiptoes.caf"
-	Typewriters        AlertSound = "typewriters.caf"
-	Update             AlertSound = "update.caf"
-)
+func NewPusher(conf *PushConfig) (*pusher, error) {
+	err := verifier.Struct(conf)
+	if err != nil {
+		return nil, err
+	}
+	return &pusher{
+		Config:   conf,
+		Contents: nil,
+	}, nil
+}
